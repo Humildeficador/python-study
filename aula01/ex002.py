57304df6dc7ca4d8077da91486ff5b756da206ee
Humildeficador <130767630+Humildeficador@users.noreply.github.com>
2023-04-16 18:21:19 -0300

    Exercicios da aula 1

---
+++ aula01/ex002.py
@@ -0,0 +1,2 @@
+n1 = int(input("Fale um número para ver seu antecessor:\n"));
+print(f'O número antecessor: {n1 - 1}');
---
+++ aula01/ex003.py
@@ -0,0 +1,6 @@
+hQnt = int(input('Digite a quantidade de horas trabalhadas:\n'));
+hValue = float(input('Digite o valor por horas trabalhadas:\n'));
+print(f'O valor total do seu salário será {(hValue * hQnt):.2f}');
+
+
+# print("a" if  4 > 3 else "b")
